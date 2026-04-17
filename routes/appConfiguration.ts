e35b3754884e31022bb15d3b1548d2b68359bf39
ashok-yannam <ashokk.reddy.yannam@gmail.com>
2026-04-17 17:16:19 +0530

    feat(ui): build avatar uploader and preview experience

--- routes/appConfiguration.ts
+++ routes/appConfiguration.ts
@@ -1,7 +1,3 @@
-/*
- * Copyright (c) 2014-2026 Bjoern Kimminich & the WowChai-Com contributors.
- * SPDX-License-Identifier: MIT
- */
 
 import { type Request, type Response } from 'express'
 import config from 'config'
